632b956ef00bd9466d6ff8d4b7eb227fdae49198
livingmeme03 <maria.lazzaro@hotmail.com>
2024-07-03 15:43:42 +0200

    Delete todolist

--- siw-food/src/main/java/it/uniroma3/siw/SiwFoodApplication.java
+++ siw-food/src/main/java/it/uniroma3/siw/SiwFoodApplication.java
@@ -10,41 +10,4 @@ public class SiwFoodApplication {
 		SpringApplication.run(SiwFoodApplication.class, args);
 	}
 	
-	
-	//TODO
-	//1. finire css
-	//3. sistemare il database definitivo e dimensioni immagini (aggiungere immagini a tutti gli ingredienti e i cuochi)
-	//5. annotazioni transactional
-	//9. optional: deploy su cloud
-	//10. optional: annotazioni internazionali per i messaggi di errore
-	// FATTO 5. mettere commenti utili nel codice sennò non si capisce niente
-	// FATTO 4. mettere in grassetto i nomi delle cose negli elenchi con i link 
-	// FATTO 6. pulsante di logout 
-	// FATTO 4. username in alto a dx su tutte le pagine
-	// FATTO 8. sistemare messaggi di errore (not blank!)
-	// FATTO 2. fare un link perenne all'index su tutte le pagine
-	// MORTO 7. aggiungere errore se viene messa una lettera al posto della quantità dell'ingrediente
-	// FATTO 1. rest
-	// FATTO 4. ricerca ricetta per nome sbagliata mi porta al login??
-	// FATTO 1. rimuovi ricette solo per nome (come aggiornamento dati? elenco con solo get?) -->attenzione mapping admin
-	// MORTO 2. rimuovi ricette che non hanno cuoco (vedi codice, è un casino col parsing) -->attenzione mapping admin
-	// FATTO 4. cuochi possono modificare/cancellare SOLO LE PROPRIE RICETTE
-	// FATTO 3. differenziare i vari ruoli con quello che possono fare e aggiungere admin al database
-	// FATTO 1. mettere unità di misura agli ingredienti (attributo e menu a tendina quando aggiungi un ingrediente)
-	// FATTO 2. binding result sulle unità di misura di ingredienti di modifica ricetta
-	// FATTO 2. ricerca ingredienti per nome		(searchmovies)
-	// FATTO ricerca ricette e cuochi
-	// FATTO 3. rimuovi ingredienti in generale
-	// FATTO 2. aggiungere ricette ai cuochi	(addactorstomovie)
-	// FATTO 2. aggiungere cuoco alla ricetta	(setdirectortomovie)
-	// FATTO 4. aggiungere ricette solo attributi semplici		(aggiungifilm)
-	// FATTO (da sistemare ingrediente) 4. rimuovi ricette/cuochi/ingredienti
-	// FATTO 6. controlli da fare su authentication col binding result
-	// FATTO 7. controllo duplicati sul login
-	// FATTO 5. rimuovi ingredienti da ricetta (credo sia insieme a quella per aggiungere ingredienti alla ricetta)
-	// FATTO 1. aggiungere ingredienti alla ricetta (nuova form) (menu a tendina con tutti gli ingredienti?) (addactorstomovie)
-	// FATTO 5. dare una schermata d'errore quando fallisce il login
-	// FATTO 3. reindirizzamento quando si clicca su risorsa proibita
-	// FATTO 11. optional: fare la roba antihacker su modifica ingredienti di una ricetta (del cuoco)
-
 }
